76cd238695783fb04fbb4b1e21991ff90eb8c3e1
MRXAZK <mrxazk@protonmail.com>
2023-01-23 03:20:45 -0500

    Move To mongodb atlas

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import pymongo
 from app.config import settings
 
 client = pymongo.MongoClient(
-    f"mongodb://{settings.MONGO_INITDB_ROOT_USERNAME}:{settings.MONGO_INITDB_ROOT_PASSWORD}@localhost:{settings.MONGO_INITDB_PORT}/{settings.MONGO_INITDB_DATABASE}?authSource=admin")
+    f"mongodb+srv://{settings.MONGO_INITDB_ROOT_USERNAME}:{settings.MONGO_INITDB_ROOT_PASSWORD}@{settings.MONGO_INITDB_HOST}/{settings.MONGO_INITDB_DATABASE}?{settings.MONGO_INITDB_OPTION}")
 db = client[settings.MONGO_INITDB_DATABASE]
 print('Connected to MongoDB...')
 User = db[f"{settings.MONGO_INITDB_COLLECTION_USER}"]
